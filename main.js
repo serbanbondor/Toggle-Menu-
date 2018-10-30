0a0bbe820bfa67f9c21b58ba174a4e3677b45ced
Serban Bondor <serbanbondor@yahoo.com>
2018-10-30 14:13:52 +0200

    js update

---
+++ main.js
@@ -0,0 +1,13 @@
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+    let getSidebarTitle = document.querySelector(".nav-sidebar ul span");
+    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+    if (toggleNavStatus === false) {
+        getSidebarUl.style.visibility = "visible";
+        getSidebar.style.width = "272px";
+    }
+}
